239ea061d8d7f4fc4a30a1689b85ca861c933111
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-23 14:36:09 +0600

    project setup

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+import mongoose from "mongoose";
+const port = 5000;
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
