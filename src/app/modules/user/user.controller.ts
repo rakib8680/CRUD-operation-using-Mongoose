c121b295742ad024fee7d61ae3dab42f5bfcacb2
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-23 18:41:21 +0600

    Create user functionality created

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,7 +3,14 @@ import { userServices } from './user.services';
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    
+    const user = req.body;
+    const result =await userServices.createUserToDB(user);
+
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    });
   } catch (error) {
     console.log(error);
   }
