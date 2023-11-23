07803697ef82bf99a22abec72314b277234ab7c6
rakib8680 <rakib.cst.1st@gmail.com>
2023-11-23 16:35:12 +0600

    eslint and prettier setup done

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express, { Request, Response } from "express";
-const app = express();
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req:Request, res:Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
-import app from "./app";
-import mongoose from "mongoose";
-import config from "./config";
+import app from './app';
+import mongoose from 'mongoose';
+import config from './config';
 
 async function main() {
-  await mongoose.connect(config.database_url as string);
-
-  app.listen(config.port, () => {
-    console.log(`Example app listening on port ${config.port}`);
-  });
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
